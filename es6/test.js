da6873af91cee0bf375e4d24c59e7118f7314317
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2021-05-31 20:38:15 -0300

    Cleaning up old files

--- es6/test.js
+++
@@ -1,9 +0,0 @@
-import cardano from '.';
-cardano.wallet.create(['Wallet Name'], {
-    'derivation-scheme': 'v2',
-    'mnemonics-length': 24,
-    'wallet-scheme': 'random_index_2levels',
-}, {
-    verbose: true,
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy90ZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLE9BQU8sT0FBTyxNQUFNLEdBQUcsQ0FBQTtBQUV2QixPQUFPLENBQUMsTUFBTSxDQUFDLE1BQU0sQ0FDcEIsQ0FBQyxhQUFhLENBQUMsRUFDZjtJQUNDLG1CQUFtQixFQUFFLElBQUk7SUFDekIsa0JBQWtCLEVBQUUsRUFBRTtJQUN0QixlQUFlLEVBQUUsc0JBQXNCO0NBQ3ZDLEVBQ0Q7SUFDQyxPQUFPLEVBQUUsSUFBSTtDQUNiLENBQ0QsQ0FBQSJ9
